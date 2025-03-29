c24775ed2a32f107dd758d6d3b8e48597e9ee046
anshshr <anshshrivastav24@gmail.com>
2025-03-29 16:42:19 +0530

    startted the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fraud_detection/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: auth()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 }
